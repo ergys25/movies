1b9d612a2f67a5d5ec906b29e63634a94c3e24ab
Ergys Hila <ergysmania@gmail.com>
2024-02-09 06:52:23 -0500

    Initial commit

---
+++ src/test/java/codes/ergyshila/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package codes.ergyshila.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
